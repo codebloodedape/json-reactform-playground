7bb93334c1510ac6c28c8a04b48e5563511a6e69
VISHAL CHANDRAN <vishhin@gmail.com>
2020-10-12 03:56:43 +0530

    Updates

--- precache-manifest.871c76c9d1f73411ba14388abcf3cd2f.js
+++ precache-manifest.871c76c9d1f73411ba14388abcf3cd2f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1d1d17dba6e98e5fcde5753140a39555",
+    "revision": "f32c0434a733ce468f85421859c70195",
     "url": "/json-reactform-playground/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/json-reactform-playground/static/css/2.2a60accc.chunk.css"
   },
   {
-    "revision": "3f1b88cae98c78bcc402",
-    "url": "/json-reactform-playground/static/css/main.30693477.chunk.css"
+    "revision": "47a7824511553fa1e7b5",
+    "url": "/json-reactform-playground/static/css/main.8a591778.chunk.css"
   },
   {
     "revision": "0316ab0462d56c140459",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/json-reactform-playground/static/js/2.54cf2362.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3f1b88cae98c78bcc402",
-    "url": "/json-reactform-playground/static/js/main.27920f4c.chunk.js"
+    "revision": "47a7824511553fa1e7b5",
+    "url": "/json-reactform-playground/static/js/main.57a5babe.chunk.js"
   },
   {
     "revision": "757af9a09e46c812c6fa",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/json-reactform-playground/precache-manifest.6b9d3805b9c065a76497e2dc0ab1b56f.js"
+  "/json-reactform-playground/precache-manifest.871c76c9d1f73411ba14388abcf3cd2f.js"
 );
 
 self.addEventListener('message', (event) => {
